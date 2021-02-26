f7abc3866c8ed2bd0dd1c6238ba1404fa26087a2
luciannebula <lucianbryan008215@gmail.com>
2021-02-26 10:56:26 -0500

    homework3.js

---
+++ homework3.js
@@ -0,0 +1,83 @@
+
+// calculate the remainder of 15/2, log the string: the remainder of 15/2 is (7.5)
+
+
+let alpha = "15"
+let bravo = "2"
+
+
+console.log(alpha)
+console.log(bravo)
+
+
+// Divide alpha/bravo -> /
+let answer = 15 / 2;
+console.log(answer,"here")
+
+let answer1 = "(7.5)"
+console.log(answer1)
+
+
+// calculate the remainder of 30/2, log the string: the remainder of 30/2 is (15)
+
+let number2 = "30"
+let number3 = "2"
+ 
+
+console.log(number2)
+console.log(number3)
+
+let number4 = 30 / 2 
+
+let remainder = "(15)"
+console.log(remainder,"15")
+
+//* using Math.random(), console.log 3 random deicimal numbers from 0 to 1000
+//* using Math.random(), console.log 3 random whole numbers from 0 to 1000
+
+
+let WeTheBest = (Math.random,Math.round)("2.5")
+let ThisForMyDogs = (Math.random,Math.round)("20.5")
+let LightersUp = (Math.random,Math.round)("10.5")
+
+
+console.log(WeTheBest)
+console.log(ThisForMyDogs)
+console.log(LightersUp)
+
+
+let AstonVantageDb9 = Math.random("100")
+let BMWe46540i = Math.random("200")
+let FerrariLaFerrari = Math.random("300")
+
+
+console.log(AstonVantageDb9)
+console.log(BMWe46540i)
+console.log(FerrariLaFerrari)
+
+// In my own words describe "type coercian" in java script = (in my opinion it the process of changing one expression or value to another ex, a number to a string  )
+
+
+// Create a program that can convert temperature you get from the user (user input).
+
+// Boiling point of water at 1 atmosphere = 212
+
+
+// Formulae Conversion Factors 9/5 = 1.8    9/4 = 2.25    10/8 = 1.25
+
+// Fahrenheit	Celsius	 C = ( F - 32) / 1.8
+
+// °F: degree Fahrenheit
+
+
+
+
+
+
+
+
+
+
+
+
+
