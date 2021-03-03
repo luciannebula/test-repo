46df64d7dcb07a567443ba5766e189418c20c5a0
luciannebula <lucianbryan008215@gmail.com>
2021-03-02 19:13:28 -0500

    updated array homework

---
+++ conditionals-exercise.js
@@ -0,0 +1,47 @@
+/* 
+    Fortune Teller Machine Project
+        (Comp Sci / Programming concept: Control Flow) -> 
+            Control Flow: The order in which individual statements, instructions or function calls are executed or evaluated.
+    
+    Goal: Create a program that spits out a random fortune everytime you run it. The program should accept a username and include it 
+        in the fortune (to personalize the message) ie: "Hello Tefe Del Rosario-Bell, your future is looking kinda bleak my friend."
+
+        How do I do this?
+        - Create a random number.
+        - Create a control flow (if..else statements) to determine what is logged to the console, based on that random number.
+        - Definitely feel free to get creative.
+*/
+
+let randomNum = 33 
+let randomNum1 = 20 
+
+
+
+
+if(randomNum = 33){
+    console.log("I finally got it right!");
+} else{
+
+
+    console.log("Nope")
+}
+
+if(randomNum1<=33){
+    console.log("your on the right path");
+}else {
+    console.log()
+}
+let username = ("lucian")
+    console.log(username)
+
+
+
+
+
+
+
+
+
+
+
+
