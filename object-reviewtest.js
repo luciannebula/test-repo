941bcaee802ea698f3fe2a074463832846ec9a17
luciannebula <lucianbryan008215@gmail.com>
2021-03-25 15:21:31 -0400

    htmlhomework

---
+++ object-reviewtest.js
@@ -0,0 +1,24 @@
+// What is a key on an object? What is a value? What is a property?
+Key is an object reference, a value is defined by a variable, a property  is a characteristic of an object, often describing attributes
+// What are a couple ways you can create an object?
+Objects are generally made up two things: properties and methods.
+// What are a couple ways you can access a key's value?
+by using this.
+// How do you assign a key-value pair to an object 
+Either we use dot notation or square bracket notation.
+// Declare a variable "human" and initilize it with a value of an object that contains relevant properties.
+
+// Create a function called covidVaccination that can take in that human object as an input and assign 
+// it a new property that identifies that human as vaccinated.
+
+// Create a constructor function for humans.
+
+// Create 5 new humans.
+
+// Vaccinate a couple of those humans and then store all of them in an array.
+
+// Create a function called covidScreener that can take in an array of humans as an input, and check to
+// see who has been vaccinated - have the function return a string identifying those humans who have and 
+// have not been vaccinated.
+
+// Delete a property from one of your humans
--- pop-quiz.js
+++ pop-quiz.js
@@ -6,10 +6,10 @@
 
 
 //1.)There are 3 javascript keywords to initialize a variable. create A new variable below called value and set it to an empty string
-let dog =  "KODA"
-const value = "pets"
+//let dog =  "KODA"
+//const value = "pets"
 //2.)JavaScript has many data types. String is an example of one. Name 2 more
-Number[0], Boolean = truthy, falsy
+//Number[0], Boolean = truthy, falsy
 //3.)Evaluate the following statements: [What would this line of code return in my console]
 //Don't run these exact line of code! [anything close is allowed]
     //4A.) console.log("4" + 4) = four plus 4 
@@ -19,26 +19,30 @@ Number[0], Boolean = truthy, falsy
     // 4E.) console.log("Javascript Is Fun".split(" "))
 
 //4.)Write a function that takes an array as input and console logs each element in the array
-const peopleInClass = [ "Shaundel Crumpton", "Lucian Bryan", "Johnnaibys Nunez", "Bayley Arens"]; 
-console.log(peopleInClass)
+//const peopleInClass = [ "Shaundel Crumpton", "Lucian Bryan", "Johnnaibys Nunez", "Bayley Arens"]; 
+//console.log(peopleInClass)
 //5.) Which of the following statements is not true about for loop statement:
 
     //A.) You can declare a variable in a for loop
     //B.) You can't make an infinite loop in a for loop = 
-    B is false 
+    //B is false 
     //C.) You can exit out of a for loop with the break keyword
     //D.) For loops can increment upwards dowards or even not at all  //ask for clarifcation if this is confusing you
 
 
 //6.) Write a function that takes an array of numbers as input and returns the average of all the numbers
-function averagebox(array){
+//const numbers = [1,2,3,4,5,6,7,8,9]
+
+//function average(array){
+    let sum = 0
     for(let i = 0; i < array.length; i++){
-        arr[i] = 
+        sum += arr[i] 
     }   
-    console.log([1, 2, 3, 4, 5,])
+    return sum / array.length
+    console.log(average(numbers));
 //7.) You are given a dataset where you have nested arrays of information and you need to sort through all that information to sum up all the numbers.
 
-function sum_array(arr) {
+//function sum_array(arr) {
   
 
   var sum = 0;
@@ -50,7 +54,7 @@ function sum_array(arr) {
       sum += arr[i][j];
       
 
-const matrix = [[1,2,[4,55],[41,455],[525,525,[13,567,96,33]],[45,4145,682]]]
+//const matrix = [[1,2,[4,55],[41,455],[525,525,[13,567,96,33]],[45,4145,682]]]
 
     //7A.) There are two potential 1 word answers for the name the approach you need to take to solve this problem. Name either one [ First one starts with I, Second one starsts with R ]
     //7B.) In 1-2 sentances explain high level [like if you were explaining to a 5 year old] what your approach to solving this would be.
