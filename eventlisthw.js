616f481ef05369f1dde86bccb35bb47ffc680ae9
luciannebula <lucianbryan008215@gmail.com>
2021-04-06 12:23:01 -0400

    eventlisthw

---
+++ eventlisthw.js
@@ -0,0 +1,25 @@
+// Add event listeners / handlers here...
+const redbutton = document.querySelector("#redbutton");
+const yellowbutton = document.querySelector("#yellowbutton");
+const skybluebutton = document.querySelector("#skybluebutton");
+const tealbutton = document.querySelector("#tealbutton");
+
+
+// Extra credit: make the border bigger on the selected circle
+// Extra credit: add a reset button - turns background white
+
+redButton.addEventListener("click", function redScheme () {
+    document.querySelector("body").className = "red-scheme";
+})
+
+skyblueButton.addEventListener("click",function skyBlueScheme(){
+    document.querySelector("body").className = "skyblue-scheme";
+})
+
+yellowButton.addEventListener("click",function yellowScheme(){
+    document.querySelector("body").className = "yellow-scheme";
+})
+
+tealbutton.addEventListener("click",function tealscheme(){
+    document.querySelector("body").className = "teal-scheme";
+})
