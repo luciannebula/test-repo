ce4e27b57e57befb9df773339cee8f87f424510f
luciannebula <lucianbryan008215@gmail.com>
2021-05-03 11:37:29 -0400

    canvasproject

--- canvas-experiment/canvas.js
+++ canvas-experiment/canvas.js
@@ -1,108 +1,128 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = 1400;
+canvas.height = 920;
+const scoreOutput = document.querySelector("#score");
+
+let score = 0;
 
 const keys = [];
 
 // load images
 
-const player= {
+const player = {
     x: 50,
-    y: 700,
+    y: 800,
     width: 40,
     height: 55,
     frameX: 0,
     frameY: 0,
-    speed: 10,
+    speed: 20,
     moving: false
 };
 
 const playerSprite = new Image();
 playerSprite.src = "images/hulk.png";
 const background = new Image();
-background.src ="images/background.jpg";
+background.src = "images/background.jpg";
 
 
-function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
 
 //let position = 0;
-
-window.addEventListener("keydown", function(e){
+// my player movement without being stuck 
+window.addEventListener("keydown", function (e) {
     keys[e.key] = true;
     player.moving = true;
 });
-window.addEventListener("keyup", function(e){
+window.addEventListener("keyup", function (e) {
     delete keys[e.key];
     player.moving = false;
 });
-
-function movePlayer(){
-    console.log(keys)
-    if (keys["ArrowUp"] && player.y > 100){
+// my player functionality/movement
+function movePlayer() {
+    //console.log(keys)
+    if (keys["ArrowUp"] && player.y > 100) {
         player.y -= player.speed;
         player.frameY = 3;
         player.moving = true;
     }
-    if (keys["ArrowLeft"] && player.x > 0){
+    if (keys["ArrowLeft"] && player.x > 0) {
         player.x -= player.speed;
         player.frameY = 1;
         player.moving = true;
     }
-    if (keys["ArrowDown"] && player.y < canvas.height - player.height){
+    if (keys["ArrowDown"] && player.y < canvas.height - player.height) {
         player.y += player.speed;
         player.frameY = 0;
         player.moving = true;
-    } 
-    if (keys["ArrowRight"] && player.x < canvas.width - player.width){
+    }
+    if (keys["ArrowRight"] && player.x < canvas.width - player.width) {
         player.x += player.speed;
         player.frameY = 2;
         player.moving = true;
-    }           
+    }
 }
-function handlePlayerFrame(){
+function handlePlayerFrame() {
     if (player.frameX < 3 && player.moving) player.frameX++
     else player.frameX = 0;
 }
-/*
-function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.
-    height);
-    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y ,player.width, player.height);
-    movePlayer();
-    handlePlayerFrame();
-    requestAnimationFrame(animate);
-}
-animate();*/
+
 
 let fps, fpsInterval, startTime, now, then, elapsed;
-
-function startAnimating(fps){
-    fpsInterval = 1000/fps;
+// animation frame to correspond with frame rate
+function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
     then = Date.now();
     startTime = then;
     animate();
 }
 
-function animate(){
+ 
+function checkCollision(player, enemy) {
+    //check player pos with enemy pos and see if they collide
+    //for (let i = 0; i < enemies.length; i++) {
+        //let enemy = enemies[i];
+        //console.log(enemy,"<--")
+        if (player.x < enemy.dx + enemy.dw &&
+            player.x + player.width > enemy.dx &&
+            player.y < enemy.dy + enemy.dh &&
+            player.y + player.height > enemy.dy) {
+            //console.log("pow!")
+            // collision detected!
+            enemy.destroy();
+        }
+    
+}
+
+function animate() {
     requestAnimationFrame(animate);
     now = Date.now()
-    elapsed = now -then;
-    if(elapsed > fpsInterval){
+    elapsed = now - then;
+    if (elapsed > fpsInterval) {
         then = now - (elapsed % fpsInterval);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(background, 0, 0, canvas.width, canvas.
-        height);
-        drawSprite(enemySprite, enemy.width * enemy.frameX, enemy.height * enemy.frameY, enemy.width, enemy.height, enemy.x, enemy.y, enemy.width, enemy.height);
-        drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y ,player.width, player.height);
+            height);
+         for(i = 0; i< enemies.length; i++){
+             let currentEnemy = enemies[i];
+             currentEnemy.moveLeft(4);
+             currentEnemy.handleFrame();
+             currentEnemy.drawSprite(ctx)
+             checkCollision(player, currentEnemy);
+         }
+        
+        drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
         movePlayer();
         handlePlayerFrame();
     }
 }
-startAnimating(10);
+
+
+startAnimating(5);
+
 
 /*const images = {};
 images.enemy = new Image();
--- canvas-experiment/enemies.js
+++ canvas-experiment/enemies.js
@@ -1,15 +1,101 @@
-const enemy= {
-    x: 200,
-    y: 700,
-    width: 60,
-    height: 55,
-    frameX: 0,
-    frameY: 0,
-    speed: 10,
-    moving: false
+const enemies =[];
+
+class Enemy {
+    constructor(image, sx, sy, swidth, sheight, dx, dy, dw, dh, speed, moving){
+        this.sx = sx;
+        this.sy = sy;
+        this.swidth = swidth;
+        this.sheight = sheight;
+        this.dx = dx;
+        this.dy = dy;
+        this.dw = dw;
+        this.dh = dh;
+        this.speed = speed;
+        this.moving = moving;
+        this.image = image;
+        this.frameY = 0;
+        this.frameX = 0;
+        enemies.push(this);
+    }
+// enemy movement onto screen
+    moveLeft(x){
+     this.dx -= x;  
+    }
+
+// when player makes contact with enemy/npc  
+    destroy(){
+      let index = enemies.indexOf(this);
+      enemies.splice(index, 1);
+        score +=  10;
+        scoreOutput.innerText = score;
+    }
+// sizing the enemy image to fit     
+    handleFrame(){
+        if(this.frameX === 0){
+            this.frameX = (enemySprite.width/3)+5;
+        }else if(this.frameX===(enemySprite.width/3)+5){
+            this.frameX = (enemySprite.width/1.5)+5;
+        }else if (this.frameX ===(enemySprite.width/1.5)+5){
+            this.frameX = 0;
+        }
+    }
+
+    returnSpriteValues(){
+        return [
+            this.image, 
+            this.sx, this.sy, 
+            this.swidth, this.sheight, 
+            this.dx, this.dy,
+            this.dw, this.dh
+        ]
+    }
+    
+
+    drawSprite(ctx){
+        ctx.drawImage(this.image, this.sx + this.frameX, this.sy, this.swidth, this.sheight, this.dx, this.dy, this.dw, this.dh);
+    }
+    
 };
 
+
 const enemySprite = new Image();
-enemySprite.src = "images/2Denemywitch.jpg";
+enemySprite.src = "images/enemysprite.png";
+
+const eVals = {
+    sx: 0,
+    sy: (enemySprite.height/20),
+    sWidth: enemySprite.width/3,
+    sHeight: enemySprite.height,
+    dx:1000,
+    dy:700,
+    dWidth: (enemySprite.width/20)/3,
+    dHeight: (enemySprite.height/20),
+}
+//console.log(enemySprite.width/20, enemySprite.height/20)
+
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
+    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
 
 
+
+const testEnemy = new Enemy(enemySprite, eVals.sx, eVals.sy, eVals.sWidth, eVals.sHeight, eVals.dx, eVals.dy, eVals.dWidth, eVals.dHeight);
+
+setInterval(newEnemies, 9000);
+
+function newEnemies(){
+    new Enemy(enemySprite, eVals.sx, eVals.sy, eVals.sWidth, eVals.sHeight, eVals.dx, eVals.dy, eVals.dWidth, eVals.dHeight)
+
+};
+//enemies.push(testEnemy);
+
+
+
+        // this.x= 300;
+        // this.y= 400;
+        // this.width= 200;
+        // this.height= 200;
+        // this.frameX= 0;
+        // this.frameY= 0;
+        // this.speed= 10;
+        // this.moving= true;
